a9007425f459199e847c7728d6d1fdd8f9545b91
Abdulloh Uyg'unov <0048242a@gmail.com>
2021-10-07 10:36:11 +0500

    boshlanishi

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print("Hello world")
+
+my_db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="abdulloh",
+    database="log_pass"
+)
+
+class Lugat:
+    def __init__(self):
+        self.uzbekcha = None
+        self.INglizcha = None
+
+    def start(self):
+        pass
+
+person = Lugat()
+person.start()
