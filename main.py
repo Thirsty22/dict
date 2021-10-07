ca0b7b5c8d925bf28e6d75eb6cab7e554a3ea22d
Abdulloh Uyg'unov <0048242a@gmail.com>
2021-10-07 15:27:32 +0500

    Estan chiqan tanlash qismi qo'shildi!

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
-
 import mysql.connector
-password = "123456789"
+
+password = "abdulloh"
 db = "lugat"
 
 my_db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password= password,
-    database= db
+    host = "localhost",
+    user = "root",
+    password = password,
+    database = db
 )
 
 tablename = "lugat"
@@ -19,24 +19,51 @@ class Lugat:
         self.uzbekcha = None
         self.Inglizcha = None
 
-    def start(self):
+    def tanlash(self):
+        self.clear()
+        print("""
+********************************************
+* Yangi so'z qo'shish                  [1] *
+* Lug'at ichidagi so'zlarni ko'rish    [2] *
+* Izlash                               [3] *
+* Chiqish                              [4] *
+********************************************
+        """)
+        option = ['1', '2', '3', '4']
+        input_num = input("[1-4]: ").strip()
+
+        while input_num not in option:
+            self.clear()
+            print("Bunaqa raqam yo'q!")
+            input_num = input("[1-4]: ")
+
+        if input_num == option[0]:
+            self.qoshishqismi()
+        elif input_num == option[1]:
+            print("Lug'at ichidagi so'zlarni ko'rish")
+        elif input_num == option[2]:
+            self.izlash()
+        else:
+            exit()
+
+    def qoshishqismi(self):
         print("Lug`atga so`z kiriting")
-        Uzbek = input("Uzbek: ").strip()
+        uzbek = input("Uzbek: ").strip().lower()
 
-        while not self.empty_string(Uzbek.isalpha()):
+        while not self.empty_string(uzbek.isalpha()):
             self.clear()
             print("So'z kiriting son va belgilar kirintmang")
-            Uzbek = input("Uzbek: ").strip()
+            uzbek = input("Uzbek: ").strip()
 
 
-        English = input("English: ").strip()
+        english = input("English: ").strip().lower()
 
-        while not self.empty_string(English.isalpha()):
+        while not self.empty_string(english.isalpha()):
             self.clear()
             print("So'z kiriting son va belgilar kirintmang")
-            English = input("English: ").strip()
+            english = input("English: ").strip()
 
-        self.assigh_user_values(tablename, Uzbek, English)
+        self.assigh_user_values(tablename, uzbek, english)
 
     def izlash(self):
         self.clear()
@@ -46,7 +73,7 @@ class Lugat:
             self.clear()
             print("So'z kiriting son va belgilar kirintmang")
             soz = input("So`zni kiriting: ")
-        print("7")
+
 
 
     def empty_string(self, string):
@@ -57,7 +84,7 @@ class Lugat:
         os.system("clear")
 
 
-    def assigh_user_values(self,table_name,  uzbek, english):
+    def assigh_user_values(self, table_name, uzbek, english):
         mycursor = my_db.cursor()
         mycursor.execute(
             f"insert into {table_name} (uzbek, english) values ('{uzbek}', '{english}');")
@@ -66,7 +93,7 @@ class Lugat:
     def izlash_uchun(self, table_name, title, info):
         mycursor = my_db.cursor()
         mycursor.execute(f"select * from {table_name} where {title} = '{info}';")
-        mycursor_1=mycursor.fetchall()
+        mycursor_1 = mycursor.fetchall()
 
         if mycursor_1:
             return True
@@ -74,5 +101,7 @@ class Lugat:
 
 
 
+
+
 person = Lugat()
-person.start()
+person.tanlash()
